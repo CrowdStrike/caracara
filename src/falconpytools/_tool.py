ee32be0490dabf65e5bd08a62799fc6f6a564df1
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-09-13 23:11:29 -0400

    [DEPLOY] Cleanup and linting

--- src/falconpytools/_tool.py
+++ src/falconpytools/_tool.py
@@ -3,6 +3,7 @@ from ._toolbox import Toolbox
 
 
 class Tool(Toolbox):
+    """Individual tool base class, inherits from the toolbox"""
     def __init__(self, api: object = None, verbose: bool = True):
         super().__init__(api=api, verbose=verbose)
         self.api = api
