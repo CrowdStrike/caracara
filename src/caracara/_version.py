76f2847a99863b23b966b26a37ccfc43abc7f43b
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-11-27 20:02:12 -0500

    Bump version -> 0.0.33

--- src/caracara/_version.py
+++ src/caracara/_version.py
@@ -35,7 +35,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 For more information, please refer to <https://unlicense.org>
 """
-_VERSION = '0.0.32'
+_VERSION = '0.0.33'
 _MAINTAINER = 'Joshua Hiller'
 _AUTHOR = 'CrowdStrike'
 _AUTHOR_EMAIL = 'falconpy@crowdstrike.com'
