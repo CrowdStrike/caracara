09263a2db8a2032d7931017562f355ac28916d1f
Joshua Hiller <joshua.hiller@crowdstrike.com>
2021-11-27 22:31:54 -0500

    Add kittype class

---
+++ src/caracara/_kits.py
@@ -0,0 +1,8 @@
+"""Kits class defines the available Toolboxes."""
+from enum import Enum
+
+
+class Kits(Enum):
+    """Enumerator for toolbox class name lookups."""
+    HOSTS = "HostsToolbox"
+    RTR = "RTRToolbox"
