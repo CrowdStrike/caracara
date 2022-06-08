050c5af7dd4f8b7fcb97feb0c4be1c807bd542ce
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-06-08 07:11:31 -0400

    :loud_sound: Add resolved base_url logging

--- caracara/client.py
+++ caracara/client.py
@@ -133,6 +133,7 @@ class Client:
         else:
             raise Exception("Impossible authentication scenario")
 
+        self.logger.info("Resolved Base URL: %s", self.api_authentication.base_url)
         # Configure modules here so that IDEs can pick them up
         self.logger.debug("Setting up Hosts module")
         self.hosts = HostsApiModule(self.api_authentication)
