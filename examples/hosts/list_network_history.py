d39d516e50e5555452622b89b17f5275036bcaec
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-06-09 09:56:14 -0400

    :sparkles: New Example: List Network History

---
+++ examples/hosts/list_network_history.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Caracara Examples Collection.
+
+list_network_history.py
+
+This example will use the API credentials configured in your config.yml file to
+list the network history for systems within your Falcon tenant.
+
+The example demonstrates how to use the Hosts API.
+"""
+import logging
+
+from caracara import Client
+
+from examples.common import caracara_example, NoDevicesFound, NoAddressesFound
+
+
+@caracara_example
+def list_network_history(**kwargs):
+    """List All Devices."""
+    client: Client = kwargs['client']
+    logger: logging.Logger = kwargs['logger']
+
+    logger.info("Listing all network address changes within the tenant")
+
+    with client:
+        response_data = client.hosts.describe_network_address_history()
+
+    for device_id, device_data in response_data.items():
+        recents = device_data.get("history", "No recent changes")
+        changes = "No changes found"
+        found = []
+        if recents:
+            for change in recents:
+                change_detail = "".join([
+                    f"{change.get('ip_address', 'IP Unknown')} ",
+                    f"({change.get('mac_address', 'MAC Unknown')}) on ",
+                    f"{change.get('timestamp', 'Unknown')}"
+                    ])
+                if change_detail not in found:
+                    found.append(change_detail)
+            changes = ", ".join(found)
+        logger.info("%s (%s)", device_id, changes)
+
+    logger.info("Found %d devices", len(response_data))
+    if not response_data:
+        raise NoDevicesFound
+    if not found:
+        raise NoAddressesFound
+
+
+if __name__ in ["__main__", "examples.hosts.list_network_history"]:
+    try:
+        list_network_history()
+        raise SystemExit
+    except NoDevicesFound as no_devices:
+        raise SystemExit(no_devices) from no_devices
+    except NoAddressesFound as no_addresses:
+        raise SystemExit(no_addresses) from no_addresses
