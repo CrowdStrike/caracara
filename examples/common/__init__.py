3d2756c1e98a91dcc6854942848a1424a1c6718d
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-06-09 20:08:00 -0400

    :children_crossing: Add example execution timer class

--- examples/common/__init__.py
+++ examples/common/__init__.py
@@ -8,11 +8,12 @@ __all__ = [
 from examples.common.example import caracara_example
 from examples.common.filter_loader import parse_filter_list
 from examples.common.utils import pretty_print
+from examples.common.timer import Timer
 from examples.common.exceptions import (
     MissingArgument, NoDevicesFound, NoSessionsConnected, NoLoginsFound, NoAddressesFound
 )
 
 __all__ = [
     "MissingArgument", "NoDevicesFound", "NoSessionsConnected", "caracara_example",
-    "parse_filter_list", "pretty_print", "NoLoginsFound", "NoAddressesFound"
+    "parse_filter_list", "pretty_print", "NoLoginsFound", "NoAddressesFound", "Timer"
     ]
---
+++ examples/common/timer.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from math import ceil
+
+class Timer():
+    """Timer class to track example execution times."""
+
+    def __init__(self):
+        """Initialize the timer."""
+        self.start = datetime.utcnow()
+        self.divider = 1000000
+
+    def __str__(self):
+        """Return current progressed time in seconds as a string."""
+        return f"{float((datetime.utcnow() - self.start).microseconds / self.divider)}"
+
+    def __float__(self):
+        """Return current progressed time in seconds as a float."""
+        return float((datetime.utcnow() - self.start).microseconds / self.divider)
+
+    def __int__(self):
+        """Return current progressed time in seconds as an integer (rounded up)."""
+        return ceil((datetime.utcnow() - self.start).microseconds / self.divider)
