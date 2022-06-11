9e2348873a7b87838a8b5c5f8491a77ce67594cc
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-06-11 14:28:45 -0400

    ✨ Add host group CRUD functionality

--- caracara/modules/hosts/hosts.py
+++ caracara/modules/hosts/hosts.py
@@ -30,6 +30,8 @@ from caracara.common.exceptions import (
     HostGroupNotFound,
     DeviceNotFound,
     MustProvideFilter,
+    MissingArgument,
+    MissingArguments,
 )
 from caracara.common.module import FalconApiModule
 from caracara.common.pagination import (
@@ -421,7 +423,149 @@ class HostsApiModule(FalconApiModule):
         if returned["errors"]:
             raise GenericAPIError(returned["errors"])
 
-        return returned
+        return returned["body"]["resources"]
+
+    def create_group(self,
+                     group_name: str = None,
+                     description: str = None,
+                     group_type: str = "static",
+                     assignment_rule: str = None
+                     ) -> Dict:
+        """Create a group based upon the provided arguments within your Falcon tenant.
+
+        Arguments
+        ---------
+        assignment_rule: str, optional - Assignment rule to apply when creating the host group.
+        description: str, optional - Description to use for the created host group.
+        group_name: str, required - Name of the host group to create.
+        group_type: str, optional - Type of host group to create. `dynamic` or `static`.
+            Defaults to `static`.
+
+        Returns
+        -------
+        dict: A dictionary containing details for the host group creation result.
+        """
+        return self._create_host_group(
+            group_name,
+            description,
+            group_type,
+            assignment_rule
+        )
+
+    def update_group(self,
+                     group_id: str = None,
+                     group_name: str = None,
+                     group_description: str = None,
+                     assignment_rule: str = None
+                     ) -> Dict:
+        """Update the name, assignment rule or description for a Host Group wihtin your tenant.
+
+        Arguments
+        ---------
+        assignment_rule: str, optional - Assignment rule to apply when creating the host group.
+        description: str, optional - Description to use for the created host group.
+        group_name: str, optional - Name of the host group to create.
+        group_id: str, required - ID of the host group to be updated.
+
+        Returns
+        -------
+        dict: A dictionary containing details for the host group creation result.
+        """
+        if not group_id:
+            raise MissingArgument("group_id")
+        if not group_name and not assignment_rule and not group_description:
+            raise MissingArguments(["group_name", "group_description", "assignment_rule"])
+
+        return self.host_group_api.update_host_groups(
+            id=group_id,
+            assignment_rule=assignment_rule,
+            description=group_description,
+            name=group_name
+        )["body"]["resources"]
+
+    def group(self,
+              group_name: str = None,
+              description: str = None,
+              assignment_rule: str = None,
+              device_ids: List[str] = None
+              ) -> Dict:
+        """Groups a list of device IDs into a newly created host group within the tenant.
+
+        Arguments
+        ---------
+        assignment_rule: str, optional - Assignment rule to apply when creating the host group.
+        description: str, optional - Description to use for the created host group.
+        group_name: str, required - Name of the host group to create.
+        device_ids: List[str], required - List of device IDs to add to the newly created group.
+
+        Returns
+        -------
+        dict: A dictionary containing details for the host group creation result.
+        """
+        if not description:
+            description = "Grouped collection of hosts"
+        if not device_ids:
+            raise MissingArgument("device_ids")
+        new_group = self._create_host_group(group_name,
+                                            description,
+                                            "static",
+                                            assignment_rule
+                                            )[0]["id"]
+        return self.add_to_group(group_ids=new_group, device_ids=device_ids)["resources"]
+
+    def ungroup(self, group_ids: List[str] or str = None, remove_groups: bool = False):
+        """Removes all members from host groups and then removes the group.
+
+        Arguments
+        ---------
+        group_ids: List[str] or str, required - List of host group IDs to ungroup.
+        remove_groups: bool, optional - Should host groups be removed after ungrouping.
+
+        Returns
+        -------
+        dict: A dictionary containing details for the host group update results.
+        """
+        if not group_ids:
+            raise MissingArgument("group_ids")
+        returned = {}
+        if isinstance(group_ids, str):
+            group_ids = group_ids.split(",")
+        for group in group_ids:
+            members = self.get_group_member_ids(group)
+            returned[group] = {}
+            returned[group]["removed"] = False
+            if members:
+                returned[group]["result"] = self.remove_from_group(group_ids=group, device_ids=members)
+                self.logger.info("Removed %d members from group %s", len(returned[group]["result"]), group)
+            if remove_groups:
+                self.logger.info("Remove group %s", group)
+                delete_result = self.delete_group(group_ids=group)
+                if delete_result:
+                    returned[group]["removed"] = True
+
+        return returned["resources"]
+
+    def _create_host_group(self,
+                     group_name: str = None,
+                     description: str = None,
+                     group_type: str = "static",
+                     assignment_rule: str = None
+                     ) -> Dict:
+        """Create the requested host group within the tenant."""
+        if not group_name:
+            raise MissingArgument("group_name")
+        if not group_type:
+            self.logger.info("Group type not specified for creation, defaulting to static.")
+
+        returned = self.host_group_api.create_host_groups(name=group_name,
+                                                      description=description,
+                                                      group_type=group_type,
+                                                      assignment_rule=assignment_rule
+                                                      )["body"]
+        if returned["errors"]:
+            raise GenericAPIError(returned["errors"])
+
+        return returned["resources"]
 
     def _perform_action(self, action_name: str, device_ids: List[str]) -> Dict:
         """Perform the specified action against the list of targets."""
@@ -533,7 +677,7 @@ class HostsApiModule(FalconApiModule):
             limit=HOST_GROUP_SCROLL_BATCH_SIZE
         )
         if not id_list:
-            raise DeviceNotFound
+            raise HostGroupNotFound
 
         return id_list
 
@@ -556,7 +700,5 @@ class HostsApiModule(FalconApiModule):
             logger=self.logger,
             limit=HOST_GROUP_SCROLL_BATCH_SIZE
         )
-        if not id_list:
-            raise HostGroupNotFound
 
         return id_list
