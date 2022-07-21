5b4d6c1b0b351c54f9b0049308e8bbb8e0e35f24
Sam Wedgwood <sam@wedgwood.dev>
2022-07-21 16:24:14 +0100

    Added tests for `PreventionPoliciesApiModule`

--- tests/unit_tests/test_prevention_policies.py
+++ tests/unit_tests/test_prevention_policies.py
@@ -1,14 +1,9 @@
 """Unit tests for PreventionPoliciesApiModule"""
+import copy
 from unittest.mock import patch
 import falconpy
-from caracara import Client
+from caracara import Client, Policy
 from caracara.common.sorting import SORT_ASC
-
-# A lot of mock methods need a certain function signature, and since they only mock functionality
-# they do not always use all the arguments. So we disable the unused-argument warning.
-# We also disable the redefined-builtin warning, as falconpy uses builtin identifiers like 'filter'
-# so we must use it also in order to mock it.
-# pylint: disable=unused-argument, redefined-builtin
 
 
 def prevpol_test():
@@ -37,71 +32,103 @@ def prevpol_test():
     return decorator
 
 
-# @prevpol_test()
-# def test_describe_devices(auth: Client, **_):
-#     """Unit test for HostsApiModule.describe_devices"""
-#     # Mock falconpy methods
-#     auth.hosts.hosts_api.configure_mock(**{
-#         "query_devices_by_filter_scroll.side_effect": mock_query_devices_by_filter_scroll,
-#         "get_device_details.side_effect": mock_get_device_details,
-#     })
+# Disabling this warning because these are not publicly shared constants, they're just locally
+# shared test variables. The reused mock function also does not need a docstring, as it's purpose
+# is conveyed by the function it's mocking.
+# pylint: disable=invalid-name,missing-function-docstring
+test_filters = None
+test_sort = SORT_ASC
 
-#     visible_devices = dict(
-#         (id_, dev) for id_, dev in mock_devices.items() if dev.get("host_hidden_status") != "hidd
-#     )
-#     assert auth.hosts.describe_devices() == visible_devices
+mock_policies = [
+    {
+        "id": "00000000000000000000000000000000",
+        "cid": "00000000000000000000000000000001",
+        "name": "Test Policy",
+        "description": "Test desc",
+        "platform_name": "Mac",
+        "groups": [],
+        "enabled": True,
+        "created_by": "test.person@crowdstrike.test",
+        "created_timestamp": "2019-02-14T19:57:16.315431783Z",
+        "modified_by": "test.person@crowdstrike.test",
+        "modified_timestamp": "2019-02-14T19:57:16.315431783Z",
+        "prevention_settings": [],
+        "ioa_rule_groups": [],
+    },
+]
+
+
+def mock_query_combined_policies(filter, sort, offset, limit):
+    assert filter == test_filters
+    assert sort == test_sort
+
+    return {
+        "body": {
+            "resources": mock_policies[offset:offset+limit],
+            "meta": {
+                "pagination": {
+                    "total": len(mock_policies)
+                }
+            }
+        }
+    }
 
 
 @prevpol_test()
 def test_describe_policies_raw(auth: Client, **_):
     """Unit test for PreventionPoliciesApiModule.describe_policies_raw"""
-    test_filters = None
-    test_sort = SORT_ASC
-
-    # NOTE: This is not structured like an real result from the API
-    resources = [
-        "Not",
-        "Real",
-        "Test",
-        "Response"
-    ]
-
-    def mock_query_combined_policies(filters, sort, offset, limit):
-        assert filters == test_filters
-        assert sort == test_sort
-
-        return {
-            "body": {
-                "resources": resources[offset:offset+limit],
-                "meta": {
-                    "pagination": {
-                        "total": len(resources)
-                    }
-                }
-            }
-        }
-        pass
 
     auth.prevention_policies.prevention_policies_api.configure_mock(**{
         "query_combined_policies.side_effect": mock_query_combined_policies
     })
 
-    assert auth.prevention_policies.describe_policies_raw() == resources
+    assert auth.prevention_policies.describe_policies_raw(
+        filters=test_filters, sort=test_sort
+    ) == mock_policies
 
 
 @prevpol_test()
 def test_describe_policies(auth, **_):
     """Unit test for PreventionPoliciesApiModule.describe_policies"""
-    assert False
+
+    auth.prevention_policies.prevention_policies_api.configure_mock(**{
+        "query_combined_policies.side_effect": mock_query_combined_policies
+    })
+
+    results = auth.prevention_policies.describe_policies(
+        filters=test_filters, sort=test_sort
+    )
+
+    result_dumps = [pol.dump() for pol in results]
+
+    assert result_dumps == mock_policies
 
 
 @prevpol_test()
-def test_new_policy(auth, **_):
-    """Unit test for PreventionPoliciesApiModule.new_policy"""
-    assert False
-
-
-@prevpol_test()
-def test_push_policy(auth, **_):
+def test_push_policy(auth: Client, **_):
     """Unit test for PreventionPoliciesApiModule.push_policy"""
-    assert False
+
+    mock_policy = {
+        "name": "Test policy",
+        "platform_name": "Windows",
+        "description": "Test policy description",
+        "prevention_settings": [],
+    }
+    mock_cid = "00000000000000000000000000000001"
+
+    def mock_create_policies(body):
+        body = copy.deepcopy(body)  # must deep copy for assert_called_once_with to work correctly
+        body["resources"][0]["cid"] = mock_cid
+        return {"body": body}
+
+    auth.prevention_policies.prevention_policies_api.configure_mock(**{
+        "create_policies.side_effect": mock_create_policies
+    })
+
+    res = auth.prevention_policies.push_policy(Policy(data_dict=mock_policy, style="prevention"))
+
+    assert res.cid == mock_cid
+
+    auth.prevention_policies.prevention_policies_api.create_policies.assert_called_once_with(
+        body={"resources": [mock_policy]}
+    )
