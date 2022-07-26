5be884bb4b45092e56089650f347be1525c24588
Chris Hammond <chris.hammond@crowdstrike.com>
2022-07-26 16:43:56 -0400

    Expose InnerRTRBatchSession too

--- caracara/modules/rtr/__init__.py
+++ caracara/modules/rtr/__init__.py
@@ -3,12 +3,14 @@ __all__ = [
     'BatchGetCmdRequest',
     'FILTER_ATTRIBUTES',
     'GetFile',
+    'InnerRTRBatchSession',
     'RTRApiModule',
     'RTRBatchSession',
 ]
 
 from caracara.modules.rtr.batch_session import (
     BatchGetCmdRequest,
+    InnerRTRBatchSession,
     RTRBatchSession,
 )
 from caracara.modules.rtr.get_file import GetFile
