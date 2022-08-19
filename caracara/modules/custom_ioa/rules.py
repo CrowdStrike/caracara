c86da9e3f272f455be7f73618a32c5a3b675f8f2
Sam Wedgwood <sam@wedgwood.dev>
2022-08-19 01:51:34 +0100

    Removed unnecessary import

--- caracara/modules/custom_ioa/rules.py
+++ caracara/modules/custom_ioa/rules.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 from datetime import datetime
 from enum import Enum
-from os import name
 from typing import Dict, List, Optional
 
 from caracara.modules.custom_ioa.rule_types import RuleType
